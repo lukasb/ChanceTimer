566ded15bf2263825fa0c856d7bc1f3e2757c585
lukasb <lukasb@gmail.com>
2024-02-07 16:45:50 -0800

    Initial Commit

---
+++ ChanceTimer/ChanceTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChanceTimerApp.swift
+//  ChanceTimer
+//
+//  Created by Lukas on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ChanceTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
